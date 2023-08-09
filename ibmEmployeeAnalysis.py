99df45512e6adc6d8af39043357a08fb720e9859
rujvid <Rujvi.doppalapudi@gmail.com>
2023-08-09 18:01:35 -0500

    [1]: add new files

---
+++ ibmEmployeeAnalysis.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import ttest_ind
+from scipy.stats import chi2_contingency
+
+
+# Loading and exploring data
+# Read the dataset
+data = pd.read_csv("IBM employee data.csv")
+
+# Display basic info about the dataset
+print(data.info())
+
+# Display the first few rows of the dataset
+print(data.head())
+
+# Check for missing values
+print(data.isnull().sum())
+
+# Data Processing
+# Encode categorical variables using one-hot encoding
+data_encoded = pd.get_dummies(data, drop_first=True)
+
+# Summary statistics
+print(data_encoded.describe())
+
+
+
+# Data Analysis and visualization
+# Attrition Analysis
+# What is the overall attrition rate in the company?
+# Are there any specific departments or job roles with higher attrition rates?
+# How does attrition vary based on education levels and fields?
+
+# Calculate overall attrition rate
+attrition_rate = data['Attrition'].value_counts(normalize=True) * 100
+print("Overall Attrition Rate:\n", attrition_rate)
+
+# Attrition by department
+dept_attrition = data.groupby('Department')['Attrition'].value_counts(normalize=True).unstack() * 100
+print("Attrition by Department:\n", dept_attrition)
+
+# Attrition by job role
+role_attrition = data.groupby('JobRole')['Attrition'].value_counts(normalize=True).unstack() * 100
+print("Attrition by Job Role:\n", role_attrition)
+
+# Attrition by education level
+education_attrition = data.groupby('EducationField')['Attrition'].value_counts(normalize=True).unstack() * 100
+print("Attrition by Education Field:\n", education_attrition)
+
+# Visualization of overall attrition rate
+plt.figure(figsize=(6, 4))
+sns.countplot(x='Attrition', data=data)
+plt.title("Overall Attrition Rate")
+plt.xlabel("Attrition")
+plt.ylabel("Count")
+plt.show()
+
+# Visualization of attrition by department
+plt.figure(figsize=(10, 6))
+sns.countplot(x='Department', hue='Attrition', data=data)
+plt.title("Attrition by Department")
+plt.xlabel("Department")
+plt.ylabel("Count")
+plt.legend(title="Attrition")
+plt.show()
+
+# Visualization of attrition by job role
+plt.figure(figsize=(12, 8))
+sns.countplot(y='JobRole', hue='Attrition', data=data)
+plt.title("Attrition by Job Role")
+plt.xlabel("Count")
+plt.ylabel("Job Role")
+plt.legend(title="Attrition")
+plt.show()
+
+# Visualization of attrition by education field
+plt.figure(figsize=(10, 6))
+sns.countplot(y='EducationField', hue='Attrition', data=data)
+plt.title("Attrition by Education Field")
+plt.xlabel("Count")
+plt.ylabel("Education Field")
+plt.legend(title="Attrition")
+plt.show()
+
+
+# Is there a correlation between age and attrition? Do younger or older employees tend to leave more?
+# How does the average age differ between employees who left and those who stayed?
+# Correlation between age and attrition
+# Average age comparison
+average_age_left = data[data['Attrition'] == 'Yes']['Age'].mean()
+average_age_stayed = data[data['Attrition'] == 'No']['Age'].mean()
+
+print("Average age of employees who left:", average_age_left)
+print("Average age of employees who stayed:", average_age_stayed)
+
+# Visualization of correlation between age and attrition
+sns.boxplot(x='Attrition', y='Age', data=data)
+plt.title("Correlation between Age and Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Age")
+plt.show()
+
+
+
+# Is there a correlation between job satisfaction and attrition?
+# Do employees with lower job satisfaction tend to leave more?
+
+# Calculate the correlation coefficient
+correlation = data_encoded['JobSatisfaction'].corr(data_encoded['Attrition_Yes'])
+
+print("Correlation between job satisfaction and attrition:", correlation)
+
+# Box plot of job satisfaction by attrition
+plt.figure(figsize=(8, 6))
+sns.boxplot(x='Attrition_Yes', y='JobSatisfaction', data=data_encoded)
+plt.title("Job Satisfaction vs. Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Job Satisfaction")
+plt.show()
+
+
+# Does monthly income have an impact on attrition?
+# Do employees with lower income leave more frequently?
+
+# Remove unnecessary columns
+data = data.drop(['EmployeeNumber', 'Over18', 'StandardHours'], axis=1)
+
+# Encode categorical variables using one-hot encoding
+data_encoded = pd.get_dummies(data, drop_first=True)
+
+# Box plot of MonthlyIncome vs Attrition
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='Attrition_Yes', y='MonthlyIncome', data=data_encoded)
+plt.title("Monthly Income vs Attrition")
+plt.xlabel("Attrition")
+plt.ylabel("Monthly Income")
+plt.show()
+
+# Perform t-test to compare MonthlyIncome for employees who left and stayed
+# The t-test helps us determine whether the difference in means is statistically significant
+# or could have occurred by chance. The t-statistic and p-value from the test provide insights
+# into the impact of monthly income on attrition.
+attrition_yes = data_encoded[data_encoded['Attrition_Yes'] == 1]['MonthlyIncome']
+attrition_no = data_encoded[data_encoded['Attrition_Yes'] == 0]['MonthlyIncome']
+t_statistic, p_value = ttest_ind(attrition_yes, attrition_no)
+
+print("T-Statistic:", t_statistic)
+print("P-Value:", p_value)
+
+
+
+# Is there a correlation between work-life balance and attrition?
+# Do employees with poor work-life balance leave more often?
+
+
+
--- try2.py
+++ try2.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -44,3 +45,51 @@ plt.xlabel("Age Group")
 plt.ylabel("Number of Suicides")
 plt.show()
 
+=======
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Step 1: Load the data and explore its structure
+data = pd.read_csv("suicides_India.csv")
+
+# Display basic information about the dataset
+print(data.info())
+
+# Step 2: Data cleaning and preprocessing (if required)
+# Check for missing values
+print(data.isnull().sum())
+
+# Handle missing values if any
+# For example, you can fill missing values in a particular column with its mean or mode.
+
+# Step 3: Exploratory Data Analysis (EDA)
+# Here, you can explore the dataset to understand its content better.
+# For example, check the columns, their data types, unique values, etc.
+print(data.head())
+
+total_suicides = len(data)
+print("Total number of suicide cases:", total_suicides)
+
+common_reasons = data['Type'].value_counts().head(5)  # Change 5 to any desired number
+print("Most common reasons for suicide:\n", common_reasons)
+
+# Calculate suicide rates by state (using raw counts)
+state_suicides = data['State'].value_counts()
+
+highest_suicide_states = state_suicides.nlargest(5)  # Change 5 to any desired number
+lowest_suicide_states = state_suicides.nsmallest(5)   # Change 5 to any desired number
+
+print("States with highest suicide rates:\n", highest_suicide_states)
+print("\nStates with lowest suicide rates:\n", lowest_suicide_states)
+
+# Create a pivot table for number of suicides across age and gender
+pivot_age_gender = data.pivot_table(index='Age_group', columns='Gender', aggfunc='size')
+
+# Plot the pivot table as a bar plot
+pivot_age_gender.plot(kind='bar', figsize=(10, 6))
+plt.title("Number of Suicides Across Age Groups and Gender")
+plt.xlabel("Age Group")
+plt.ylabel("Number of Suicides")
+plt.show()
+
+>>>>>>> Stashed changes
